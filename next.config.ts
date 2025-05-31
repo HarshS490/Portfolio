4a0de2e8a5997ff52c7c7f6b19b0e7809402febc
Harsh Singh <harsh.singh30112004@gmail.com>
2025-06-01 01:22:24 +0530

    changing remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns: [new URL('https://picsum.photos/**'), new URL("https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/**")]
+  }
 };
 
 export default nextConfig;
